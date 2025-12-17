9322894522df085ccab1317580723fb8360d405b
bala-amypo <bala@amypo.in>
2025-12-17 06:06:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,8 +1,18 @@
 package com.example.aiml.entity;
+
+import jakarta.persistence.*;
+
+
+@entity
+//@Table(name = "students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id ;
     private String name;
     private String email;
+    
     public void setId(long id){
         this.id=id;
     }
